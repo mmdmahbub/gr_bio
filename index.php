b652630fc75ed62dcbc901e7725ef827d670db22
Mahbub Ansary <mdmahbub903@gmail.com>
2018-08-28 16:20:41 +0600

    safe commit

--- index.php
+++ index.php
@@ -30,17 +30,552 @@
 	 <link rel="stylesheet" href="assets/css/responsive.css">
 </head>
 <body>
+	<!--
+		***************
+		Landing Screen  starts here
+		***************
+	-->
 	<div class="header_area bg devider">
-			<div class="overlay"></div>
-			<div class="display_table_cell">
-				<div class="container">
-					<div class="intro_text">
-						<h1>I'm Golam Rabbani</h1>
-						<p>Student, Politician, Worker </p>
+		<div class="overlay"></div>
+		<div class="display_table_cell">
+			<div class="container">
+				<div class="intro_text">
+					<h1>I'm Golam Rabbani</h1>
+					<p>Student, Politician &amp Worker </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		Nlanding Screen ends here
+		***************
+	-->
+	<!--
+		***************
+		Navigation Bar starts here
+		***************
+	-->
+	<nav class="my_nav">
+		<div class="container">
+			<div class="mbl_nav_toggler">
+				<i class="fa fa-bars"></i>
+			</div>
+			<ul class="mobile_nav">
+				<li><a href="" class="active">About</a></li>
+				<li><a href="">Skills</a></li>
+				<li><a href="">Education</a></li>
+				<li><a href="">Project</a></li>
+				<li><a href="">Awards</a></li>
+				<li><a href="">Gallery</a></li>
+				<li><a href="">Portfolio</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!--
+		***************
+		Navigation Bar ends here
+		***************
+	-->
+	<!--
+		***************
+		About Me starts here
+		***************
+	-->
+	<div class="about_me devider">
+		<div class="container">
+			<div class="col-md-8">
+				<h2>Golam Rabbani</h2>
+				<span><i>Generel Secretery </i> at BCL</span>
+				<p>Student &#124; Politician &#124; Worker </p>
+				<div class="about_text">
+					Ayman Sadiq is the founder & CEO of 10 Minute School. This is the largest online education platform in Bangladesh which teaches more than 250,000 students every single day, that too completely free of cost
+					
+					<br /><br />
+					
+					Ayman Sadiq is the founder & CEO of 10 Minute School. This is the largest online education platform in Bangladesh which teaches more than 250,000 students every single day, that too completely free of cost.
+					
+					<br /> <br />
+					Ayman Sadiq is the founder & CEO of 10 Minute School. This is the largest online education platform in Bangladesh which teaches more than 250,000 students every single day, that too completely free of cost.
+				</div>
+				<div class="social_button">
+					<a href=""><i class="fa fa-facebook"></i></a> 
+					<a href=""><i class="fa fa-twitter"></i></a> 
+					<a href=""><i class="fa fa-instagram"></i></a> 
+					<a href=""><i class="fa fa-linkedin"></i></a> 
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="round_thumb">
+					<img src="assets/images/" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		About Me ends here
+		***************
+	-->
+	
+	<!--
+		***************
+		Education area starts here
+		***************
+	-->
+	<div class="education_area devider">
+		<div class="container">
+			<h1 class="dflt_header">Education</h1>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="year_title">
+						Jan, 2012 <br />
+						Dec, 2015
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="edu_details">
+						<h2>Bachelor of Business Administration</h2>
+						<span>IBA, University of Dhaka</span>
+						<p>My lessons at IBA started in the classrooms and grew into something much bigger- what started off as business hacks from the teachers grew into life and survival hacks from my friends. I participated in numerous competitions, won a few, lost many, and learnt a lot- especially from the failures. The wins taught me the result of hard work, and the losses turned out to be chances to improve and adapt</p>
 					</div>
 				</div>
 			</div>
+			<div class="row margin_top">
+				<div class="col-md-2">
+					<div class="year_title">
+						Jan, 2012 <br />
+						Dec, 2015
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="edu_details">
+						<h2>Bachelor of Business Administration</h2>
+						<span>IBA, University of Dhaka</span>
+						<p>My lessons at IBA started in the classrooms and grew into something much bigger- what started off as business hacks from the teachers grew into life and survival hacks from my friends. I participated in numerous competitions, won a few, lost many, and learnt a lot- especially from the failures. The wins taught me the result of hard work, and the losses turned out to be chances to improve and adapt</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
+	
+	<!--
+		***************
+		Education area starts ends here
+		***************
+	-->
+	<!--
+		***************
+		My Uploads starts  here
+		***************
+	-->
+	<div class="my_uploads devider">
+		<div class="container">
+			<h1 class="dflt_header">My Uploads</h1>
+			
+			<div class="row margin_bottom">
+				<div class="col-md-3">
+					<div class="video">
+						<div class="video_thumb">
+							<img src="" alt="" />
+						</div>
+						<h3 class="video_title">
+							🔥 Positivity VS Negativity | Make Positivity Louder | Ayman Sadiq (আয়মান সাদিক) 
+							
+						</h3>
+						<span>August 20, 2018</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="video">
+						<div class="video_thumb">
+							<img src="" alt="" />
+						</div>
+						<h3 class="video_title">
+							🔥 Positivity VS Negativity | Make Positivity Louder | Ayman Sadiq (আয়মান সাদিক) 
+							
+						</h3>
+						<span>August 20, 2018</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="video">
+						<div class="video_thumb">
+							<img src="" alt="" />
+						</div>
+						<h3 class="video_title">
+							🔥 Positivity VS Negativity | Make Positivity Louder | Ayman Sadiq (আয়মান সাদিক) 
+							
+						</h3>
+						<span>August 20, 2018</span>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="video">
+						<div class="video_thumb">
+							<img src="" alt="" />
+						</div>
+						<h3 class="video_title">
+							🔥 Positivity VS Negativity | Make Positivity Louder | Ayman Sadiq (আয়মান সাদিক) 
+							
+						</h3>
+						<span>August 20, 2018</span>
+					</div>
+				</div>
+				
+			</div>
+			<div class="row text-center">
+				<a href="" class="cust_btn">Subscribe Now</a>
+				<br /><br />
+			</div>
+			<div class="row">
+				<div class="big_thumb">
+				
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		My Uploads ends here
+		***************
+	-->
+	
+	<!--
+		***************
+		Awrds starts here
+		***************
+	-->
+	<div class="awrds devider">
+		<div class="container">
+			<div class="row">
+				<h1 class="dflt_header">Awards</h1>
+				<div class="row margin_bottom">
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row ">
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="gallery_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<!--
+		***************
+		Awrds ends here
+		***************
+	-->
+	<!--
+		***************
+		Gallery starts here
+		***************
+	-->
+	<div class="gallery_area devider">
+		<div class="container">
+			<div class="row margin_bottom">
+				<h1 class="dflt_header">Workshops and Training Sessions</h1>
+				<div class="row margin_bottom">
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row " >
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+							<h2>Crash Course Certificate Ceremony at Dhaka University</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<!--
+		***************
+		Gallery ends here
+		***************
+	-->
+	<!--
+		***************
+		summery_area starts here
+		***************
+	-->
+	<div class="summery_area devider">
+		<div class="container">
+			<h1 class="dflt_header">It was a bumpy ride</h1>
+			<p class="sub_head">But dreams have a tendency to play hard to get right?</p>
+			<div class="col-md-3">
+				<div class="single_ride">
+					<h5>30</h5>
+					Failed Attempts
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_ride">
+					<h5>30</h5>
+					Failed Attempts
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_ride">
+					<h5>30</h5>
+					Failed Attempts
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_ride">
+					<h5>30</h5>
+					Failed Attempts
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		summery_area ends here
+		***************
+	-->
+	<!--
+		***************
+		Workplay starts here
+		***************
+	-->
+	<div class="workplay devider">
+		<div class="container">
+			<h1 class="dflt_header">Workplay</h1>
+			<nav class="workplay_nav">
+				<ul class="">        	                                                        
+					<li class="filter-button active" data-filter="all">All</li>
+					<li class="filter-button" data-filter="dp">Digital print</li>
+					<li class="filter-button" data-filter="indr">Indoor</li>
+					<li class="filter-button" data-filter="otdr">outdoor</li>
+					<li class="filter-button" data-filter="ofst">offset</li>
+				</ul>
+			</nav>
+			<div class="row " >
+					<div class="col-md-3">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="gallery">
+							<div class="workshops_thumb">
+								<img src="" alt="" />
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+	<!--
+		***************
+		Workplay ends here
+		***************
+	-->
+	<!--
+		***************
+		Counter area ends here
+		***************
+	-->
+	<div class="counter_area devider">
+		<div class="container">
+			<div class="col-md-3">
+				<div class="single_counter">
+					<span class="number">
+						20
+					</span>
+					<br />
+					<span class="counter-text">
+						School Activations
+					</span>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_counter">
+					<span class="number">
+						20
+					</span>
+					<br />
+					<span class="counter-text">
+						School Activations
+					</span>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_counter">
+					<span class="number">
+						20
+					</span>
+					<br />
+					<span class="counter-text">
+						School Activations
+					</span>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="single_counter">
+					<span class="number">
+						20
+					</span>
+					<br />
+					<span class="counter-text">
+						School Activations
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		Counter area ends here
+		***************
+	-->
+	<!--
+		***************
+		Form Area starts here
+		***************
+	-->
+	<div class="form_area devider">
+		<div class="container">
+			<h1 class="dflt_header">Invite as a Speaker</h1>
+			
+			<div class="invitation_form">
+				<form action="">
+					<div class="col-md-6">
+						<input type="text" placeholder="Name" />
+					</div>
+					<div class="col-md-6">
+						<input type="email" placeholder="Email" />
+					</div>
+					<div class="col-md-6">
+						<input type="text" placeholder="Contact Number" />
+					</div>
+					<div class="col-md-6">
+						<input type="text" placeholder="Number of audience" />
+					</div>
+					<div class="col-md-12">
+						<input type="text" placeholder="Place" />
+					</div>
+					<div class="col-md-12">
+						<textarea name="" id="" placeholder="Details"></textarea>
+					</div>
+					<div class="col-md-12">
+						<input type="submit" value="Invite" />
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!--
+		***************
+		From Area ends here
+		***************
+	-->
+
 
 
 <!--  Footer Area  Start -->
